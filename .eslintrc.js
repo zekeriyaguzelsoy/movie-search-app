ba278be45660ba58540c2525c9af6a8066887524
Zekeriya Güzelsoy <zekeriyaguzelsoy@gmail.com>
2020-05-19 00:56:01 +0300

    Init env

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,16 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 3,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": true
+      }
+    }]
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -13,6 +13,9 @@ export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  mounted() {
+    console.log('process.env :>> ', process.env.VUE_APP_THMD_API_KEY);
   },
 };
 </script>
